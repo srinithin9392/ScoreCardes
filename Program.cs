bc8680b881804670b850893a838409fe06fe2d6a
srinithin9392 <srinithin91@gmail.com>
2026-04-19 12:41:42 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// PORT (keep this)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(int.Parse(port));
+});
 
+// Services
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// ✅ FORCE Swagger
+app.UseSwagger();
 
-app.UseHttpsRedirection();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ScoreCards API");
+    c.RoutePrefix = "swagger"; // important
+});
+
+
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Test route
+app.MapGet("/", () => "API Running...");
 
 app.Run();
---
+++ ScoreController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace ScoreCards.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoreController : ControllerBase
+    {
+        [HttpGet("GetHomeMatches")]
+        public async Task<IActionResult>Home()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://www.cricbuzz.com/api/home");
+            if(response.IsSuccessStatusCode)
+            {
+                var Home = await response.Content.ReadAsStringAsync();
+                return Ok(Home);
+             }
+             else
+             {
+                return StatusCode((int)response.StatusCode, "Error fetching score");
+            }
+        }
+        [HttpGet("")]
+        public async Task<IActionResult>GetScore()
+        {
+            var clint = new HttpClient();
+
+            var Get = await clint.GetAsync("https://www.cricbuzz.com/api/mcenter/comm/151807");
+            var Score = await Get.Content.ReadAsStreamAsync();
+
+           
+
+            return Ok(Score);
+        }
+    }
+}
